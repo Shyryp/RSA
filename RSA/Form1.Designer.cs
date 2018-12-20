269e8a42d5c8510e7126bfa0e841f2abdbfe0918
Alexandr Priyomko <sashapriyomko@mail.ru>
2018-12-20 22:33:08 +0600

    Добавьте файлы проекта.

---
+++ RSA/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace RSA
+{
+    partial class RSA
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxOne = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.privateKey = new System.Windows.Forms.Label();
+            this.publicKey = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxE = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxTwo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label15 = new System.Windows.Forms.Label();
+            this.textBoxOutputNoRSA = new System.Windows.Forms.TextBox();
+            this.buttonCipher = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxOutputRSA = new System.Windows.Forms.TextBox();
+            this.textBoxInputNoRSA = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxOne
+            // 
+            this.textBoxOne.Location = new System.Drawing.Point(273, 51);
+            this.textBoxOne.Name = "textBoxOne";
+            this.textBoxOne.Size = new System.Drawing.Size(114, 23);
+            this.textBoxOne.TabIndex = 0;
+            this.textBoxOne.Text = "1201";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.buttonGenerate);
+            this.panel1.Controls.Add(this.privateKey);
+            this.panel1.Controls.Add(this.publicKey);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.textBoxE);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBoxTwo);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBoxOne);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 156);
+            this.panel1.TabIndex = 1;
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(522, 90);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(221, 39);
+            this.buttonGenerate.TabIndex = 12;
+            this.buttonGenerate.Text = "Генерировать ключи";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // privateKey
+            // 
+            this.privateKey.AutoSize = true;
+            this.privateKey.Location = new System.Drawing.Point(181, 123);
+            this.privateKey.Name = "privateKey";
+            this.privateKey.Size = new System.Drawing.Size(13, 17);
+            this.privateKey.TabIndex = 11;
+            this.privateKey.Text = "-";
+            // 
+            // publicKey
+            // 
+            this.publicKey.AutoSize = true;
+            this.publicKey.Location = new System.Drawing.Point(181, 90);
+            this.publicKey.Name = "publicKey";
+            this.publicKey.Size = new System.Drawing.Size(13, 17);
+            this.publicKey.TabIndex = 10;
+            this.publicKey.Text = "-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(32, 123);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(116, 17);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Закрытый ключ:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(32, 90);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 17);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Открытый ключ:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(589, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Введите e =";
+            // 
+            // textBoxE
+            // 
+            this.textBoxE.Location = new System.Drawing.Point(682, 48);
+            this.textBoxE.Name = "textBoxE";
+            this.textBoxE.Size = new System.Drawing.Size(61, 23);
+            this.textBoxE.TabIndex = 6;
+            this.textBoxE.Text = "17";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(420, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "q =";
+            // 
+            // textBoxTwo
+            // 
+            this.textBoxTwo.Location = new System.Drawing.Point(454, 51);
+            this.textBoxTwo.Name = "textBoxTwo";
+            this.textBoxTwo.Size = new System.Drawing.Size(104, 23);
+            this.textBoxTwo.TabIndex = 4;
+            this.textBoxTwo.Text = "1229";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(239, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "p =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите два простых числа:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(31, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Генерация ключей шифрования";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 156);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(784, 296);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.label15);
+            this.panel3.Controls.Add(this.textBoxOutputNoRSA);
+            this.panel3.Controls.Add(this.buttonCipher);
+            this.panel3.Controls.Add(this.label12);
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Controls.Add(this.textBoxOutputRSA);
+            this.panel3.Controls.Add(this.textBoxInputNoRSA);
+            this.panel3.Controls.Add(this.label10);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(784, 296);
+            this.panel3.TabIndex = 0;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(399, 152);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(147, 13);
+            this.label15.TabIndex = 3;
+            this.label15.Text = "Результат расшифрования:";
+            // 
+            // textBoxOutputNoRSA
+            // 
+            this.textBoxOutputNoRSA.Location = new System.Drawing.Point(402, 186);
+            this.textBoxOutputNoRSA.Multiline = true;
+            this.textBoxOutputNoRSA.Name = "textBoxOutputNoRSA";
+            this.textBoxOutputNoRSA.Size = new System.Drawing.Size(341, 85);
+            this.textBoxOutputNoRSA.TabIndex = 6;
+            // 
+            // buttonCipher
+            // 
+            this.buttonCipher.Location = new System.Drawing.Point(404, 67);
+            this.buttonCipher.Name = "buttonCipher";
+            this.buttonCipher.Size = new System.Drawing.Size(339, 62);
+            this.buttonCipher.TabIndex = 8;
+            this.buttonCipher.Text = "Зашифровать и расшифровать";
+            this.buttonCipher.UseVisualStyleBackColor = true;
+            this.buttonCipher.Click += new System.EventHandler(this.buttonCipher_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(32, 152);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(129, 13);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "Результат шифрования:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(32, 51);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(152, 13);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Введите шифруемую строку:";
+            // 
+            // textBoxOutputRSA
+            // 
+            this.textBoxOutputRSA.Location = new System.Drawing.Point(35, 186);
+            this.textBoxOutputRSA.Multiline = true;
+            this.textBoxOutputRSA.Name = "textBoxOutputRSA";
+            this.textBoxOutputRSA.Size = new System.Drawing.Size(318, 85);
+            this.textBoxOutputRSA.TabIndex = 4;
+            // 
+            // textBoxInputNoRSA
+            // 
+            this.textBoxInputNoRSA.Location = new System.Drawing.Point(35, 67);
+            this.textBoxInputNoRSA.Multiline = true;
+            this.textBoxInputNoRSA.Name = "textBoxInputNoRSA";
+            this.textBoxInputNoRSA.Size = new System.Drawing.Size(318, 62);
+            this.textBoxInputNoRSA.TabIndex = 3;
+            this.textBoxInputNoRSA.Text = "12345";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(35, 24);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(97, 17);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Шифрование:";
+            // 
+            // RSA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 452);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "RSA";
+            this.Text = "RSA шифрование";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxOne;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label privateKey;
+        private System.Windows.Forms.Label publicKey;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxE;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxTwo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBoxOutputNoRSA;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxOutputRSA;
+        private System.Windows.Forms.TextBox textBoxInputNoRSA;
+        private System.Windows.Forms.Button buttonCipher;
+    }
+}
+
---
+++ RSA/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace RSA
+{
+    public partial class RSA : Form
+    {
+        BigInteger ee;
+        BigInteger d;
+        BigInteger n;
+        bool bol = false;
+        public RSA()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            int numberOne = 0, numberTwo = 0;
+            numberOne = Convert.ToInt32(textBoxOne.Text);
+            numberTwo = Convert.ToInt32(textBoxTwo.Text);
+            ee = Convert.ToInt32(textBoxE.Text);
+            if (IsTheNumberSimple(numberOne) && IsTheNumberSimple(numberTwo))
+                RSA_Method_GenerateKeys(numberOne, numberTwo);
+        }
+
+        private void RSA_Method_GenerateKeys(int numberOne, int numberTwo)
+        {
+            n = numberOne * numberTwo; // 1) n = p * q
+            BigInteger fi = (numberOne - 1) * (numberTwo - 1); //2) Вычислении функции Эйлера
+            d = 1;
+            for (; ; d++) //3) вычисления d (закрытого ключа)
+            {
+                BigInteger k = ee * d;
+
+                k = k % fi;
+                
+                if (k.IsOne)
+                {
+                    break;
+                }
+            }
+            publicKey.Text = Convert.ToString(ee) + "," + Convert.ToString(n);
+            privateKey.Text = Convert.ToString(d) + "," + Convert.ToString(n);
+            bol = true;
+        }
+
+        private bool IsTheNumberSimple(BigInteger n)
+        {
+            if (n < 1)
+                return false;
+
+            if (n == 2)
+                return true;
+
+            for (BigInteger i = 2; i < n; i++)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        private void buttonCipher_Click(object sender, EventArgs e)
+        {
+            if (bol)
+            {
+                BigInteger s = Convert.ToUInt64(textBoxInputNoRSA.Text);
+                s = BigInteger.ModPow(s, ee, n);
+                textBoxOutputRSA.Text = Convert.ToString(s);
+                s = BigInteger.ModPow(s, d, n);
+                textBoxOutputNoRSA.Text = Convert.ToString(s);
+            }
+        }
+    }
+}
